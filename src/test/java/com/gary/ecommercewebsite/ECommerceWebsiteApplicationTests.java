b08ada79ad6b9fc0dbe470dc42b2301090e70de3
Gary Tang <dexife@126.com>
2019-08-15 16:28:14 +0800

    project init

---
+++ src/test/java/com/gary/ecommercewebsite/ECommerceWebsiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gary.ecommercewebsite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ECommerceWebsiteApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
