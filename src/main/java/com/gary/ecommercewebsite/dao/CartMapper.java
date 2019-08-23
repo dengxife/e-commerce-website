7bad90cd188121b32cdbe974713b86c0426f9da6
Gary Tang <dexife@126.com>
2019-08-23 17:15:31 +0800

    生成pojo、dao、mapper

---
+++ src/main/java/com/gary/ecommercewebsite/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.gary.ecommercewebsite.dao;
+
+import com.gary.ecommercewebsite.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/gary/ecommercewebsite/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.gary.ecommercewebsite.dao;
+
+import com.gary.ecommercewebsite.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
