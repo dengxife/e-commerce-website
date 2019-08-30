6894ed82b34ad106f57ed1ff713f9228aa6288e7
Gary Tang <dexife@126.com>
2019-08-30 18:44:30 +0800

    配置mybatis、logback

--- src/test/java/com/gary/ecommercewebsite/dao/UserMapperTest.java
+++ src/test/java/com/gary/ecommercewebsite/dao/UserMapperTest.java
@@ -3,6 +3,8 @@ package com.gary.ecommercewebsite.dao;
 import com.gary.ecommercewebsite.pojo.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,11 +15,14 @@ import static org.junit.Assert.*;
 @SpringBootTest
 public class UserMapperTest {
 
+    private Logger logger = LoggerFactory.getLogger(UserMapperTest.class);
+
     @Autowired
     UserMapper userMapper;
 
     @Test
     public void selectByPrimaryKey() {
+
         User user = userMapper.selectByPrimaryKey(13);
         assertEquals("geely", user.getUsername());
     }
